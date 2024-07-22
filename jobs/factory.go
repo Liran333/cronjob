5103723c719f72159bcb3b5c51dbbdc5191e8878
jackpot-yan <wikileaks928@hotmail.com>
2024-07-22 15:11:37 +0800

    reset (#17)
    Co-authored-by: jackpot <wikileaks928@gmail.com>

--- jobs/factory.go
+++ jobs/factory.go
@@ -11,6 +11,7 @@ import (
 	"github.com/openmerlin/cronjob/jobs/checkrevoke"
 	"github.com/openmerlin/cronjob/jobs/downloadcount"
 	"github.com/openmerlin/cronjob/jobs/modeldeploy"
+	"github.com/openmerlin/cronjob/jobs/moderation"
 	"github.com/openmerlin/cronjob/jobs/visitcount"
 )
 
@@ -27,6 +28,12 @@ func InitJobMap(cfg *config.Config) {
 		visitcount.JobVisitCount:       visitcount.NewVisitJob(&cfg.VisitCount),
 		checkrevoke.JobCheckUserRevoke: checkrevoke.NewCheckUserRevokeJob(),
 		modeldeploy.JobModelDeploy:     modeldeploy.NewDeployJob(&cfg.ModelDeploy, cfg.Gitee),
+		moderation.JobModerationInit:   moderation.NewModerationInitJob(&cfg.ModerationCfg),
+		moderation.JobModerationPic:    moderation.NewModerationPicJob(&cfg.ModerationCfg),
+		moderation.JobModerationDoc:    moderation.NewModerationDocJob(&cfg.ModerationCfg),
+		moderation.JobModerationVideo:  moderation.NewModerationVideoJob(&cfg.ModerationCfg),
+		moderation.JobModerationReadme: moderation.NewModerationReadmeJob(&cfg.ModerationCfg),
+		moderation.JobModerationAudio:  moderation.NewModerationAudioJob(&cfg.ModerationCfg),
 	}
 }
 
